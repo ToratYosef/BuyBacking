51168e6c629a9dc23d4348d638923c245314c9b7
ToratYosef <yoseftorat@gmail.com>
2026-02-17 17:27:16 +0000

    Temp pre-rebase commit

--- api/src/routes/refreshTracking.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { refreshTracking } = require('../../functions/index.js');
-
-const router = express.Router();
-
-router.post('/refresh-tracking', (req, res) => refreshTracking(req, res));
-
-module.exports = router;
