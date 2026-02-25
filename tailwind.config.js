ea44973457f7777be98cc05d617d7b546fe1195b
ToratYosef <yoseftorat@gmail.com>
2026-02-25 17:40:56 -0500

    Fix multi-device payout breakdown on order submitted page

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html',
+    './admin/*.html',
+    './admin/email/*.html',
+    './feeds/pages/*.html',
+    './iphone/*.html',
+    './ipad/*.html',
+    './repricer/*.html',
+    './samsung/*.html',
+    './sell/*.html',
+    './support*.html',
+    './trust/*.html',
+    './unsubscribe/*.html',
+    './assets/js/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
