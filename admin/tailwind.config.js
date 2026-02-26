8d4c6c61e6491044ce4ed76607cca4723ba7034e
ToratYosef <yoseftorat@gmail.com>
2026-02-26 17:36:01 +0000

    Auto-update Thu Feb 26 @ 17:36

--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -1,12 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    // This is the key part. It tells Tailwind to scan your HTML file.
-    "./public/admin.html", 
-    
-    // If you add other files later, you'll put them here.
-    // For example, if you add a JavaScript file for dynamic content:
-    // "./src/js/**/*.js"
+    "./**/*.html",
+    "./**/*.js",
+    "../assets/js/pages/admin*.js",
+    "../assets/js/pages/admin-*.js"
   ],
   theme: {
     extend: {},
