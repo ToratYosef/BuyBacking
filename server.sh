cfb5e32dcd3888ecfd3d9d126a18a33770e0fb7d
ToratYosef <yoseftorat@gmail.com>
2026-03-12 16:55:22 +0000

    Auto-update Thu Mar 12 @ 16:55

--- server.sh
+++ server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 PORT=${PORT:-5050}
 export PORT
+export SPA_FALLBACK=0
 echo "🚀 Starting Node server on port $PORT..."
 node server.js
--- start-server.sh
+++ start-server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 PORT=${PORT:-3000}
 export PORT
+export SPA_FALLBACK=0
 echo "🚀 Serving project ROOT (and subfolders) on port $PORT..."
 node server.js
