cf9693af85371fcac2009968eac548bdbf8e60c0
root <root@private-title.metalseed.net>
2026-03-23 15:42:23 +0000

    Auto-update Mon Mar 23 @ 15:42

--- api/scripts/analytics-retention.sql
+++
@@ -1,6 +0,0 @@
-WITH deleted AS (
-  DELETE FROM analytics_sessions
-  WHERE last_seen < NOW() - ($1::text || ' days')::interval
-  RETURNING session_id
-)
-SELECT COUNT(*)::int AS deleted_sessions FROM deleted;
